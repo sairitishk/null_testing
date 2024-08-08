ddb3607fe5b578633fad6f1d3b267a8de3779a4d
Sai Ritish Korrapati <sairitishk@gmail.com>
2024-08-08 15:51:52 +0100

    added lambda module

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "code.zip"
+  filename      = "${path.module}/code.zip"
   function_name = "HelloWorld"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
