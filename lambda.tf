a908403c465b3adfd3a542735ac17c76d1a9eaa7
Sai Ritish Korrapati <sairitishk@gmail.com>
2024-08-08 17:40:36 +0100

    updated handler and cmd for invoke

--- lambda.tf
+++ lambda.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "${path.module}/code.zip"
   function_name = "HelloWorld2"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.handler"
+  handler       = "code.lambda_handler"
   # source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
@@ -28,6 +28,7 @@ resource "null_resource" "invoke_lambda" {
     command = <<EOT
       aws lambda invoke \
         --function-name ${aws_lambda_function.test_lambda.function_name} \
+        --cli-binary-format raw-in-base64-out \
         --payload '{"key1": "value1"}' \
         response.json
       EOT
