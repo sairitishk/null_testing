b291e297a77a1bf43cfc0194b885dd497e3a63f0
Sai Ritish Korrapati <sairitishk@gmail.com>
2024-08-08 16:59:23 +0100

    added remote backend

--- provider.tf
+++ provider.tf
@@ -7,6 +7,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-backend-ritish-testing"
+    key    = "terraform.tfstate"
+    region = "us-east-1" 
+  }
 }
 
 provider "aws" {
