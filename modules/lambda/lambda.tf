7cd1da0f674a74081d4e2b84b8ca3c8ca360517f
Sai Ritish Korrapati <sairitishk@gmail.com>
2024-08-08 15:53:29 +0100

    changed lambda name

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+  name               = "iam_for_lambda_null_invoke"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
