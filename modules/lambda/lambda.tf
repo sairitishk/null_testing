eaf7c44a72c15e688cba38d3e3e10cba10f1ab69
Sai Ritish Korrapati <sairitishk@gmail.com>
2024-08-08 16:04:56 +0100

    added null resource

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -10,13 +10,35 @@ resource "aws_iam_role" "iam_for_lambda" {
 # }
 
 resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
   filename      = "${path.module}/code.zip"
   function_name = "HelloWorld"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler       = "index.handler"
   # source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
+}
+
+
+resource "null_resource" "invoke_lambda" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws lambda invoke \
+        --function-name ${aws_lambda_function.test_lambda.function_name} \
+        --payload '{"key1": "value1"}' \
+        response.json
+      EOT
+ }
+}
+
+resource "aws_lambda_permission" "allow_jenkins_invoke" {
+  statement_id  = "AllowJenkinsInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "ec2.amazonaws.com"
+  source_arn    = "arn:aws:iam::058264252836:role/Admin_Acess_Role_EC2" // Replace with your IAM role ARN
 }
